b66fd7d2c5e956fba4b4615057d3ee396408f667
ootake takanori <mine19876293@gmail.com>
2023-11-17 17:13:18 +0900

    new

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,16 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\TestController;
 use App\Http\Controllers\MiddlewareController;
 
-Route::prefix('book')->group(function () {
-    Route::get('/', [BookController::class, 'index']);
-    Route::get('/add', [BookController::class, 'add']);
-    Route::post('/add', [BookController::class, 'create']);
-    Route::get('/', [TestController::class, 'index']);
-    Route::post('/', [TestController::class, 'post']);
-    Route::get('/middleware', [MiddlewareController::class, 'index']);
-    Route::post('/middleware', [Middleware::class, 'post']);
-});
-Route::get('/relation', [AuthorController::class, 'relate']);
+use App\Http\Controllers\SessionController;
+
+Route::get('/session', [SessionController::class, 'getSes']);
+Route::post('/session', [SessionController::class, 'postSes']);
+Route::get('/middleware', [MiddlewareController::class, 'index']);
+Route::post('/middleware', [MiddlewareController::class, 'post'])->middleware('first');
+Route::get('/', [TestController::class, 'index']);
+Route::post('/', [TestController::class, 'post']);
+Route::post('/middleware', [MiddlewareController::class, 'post']);
